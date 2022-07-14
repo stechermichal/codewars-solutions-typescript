248aee520dba8e3a62b72d1433b72b8e324ffbbf
stechermichal <stechermichal@gmail.com>
2022-07-14 16:37:19 +0200

    Finished 'Transportation on vacation'

---
+++ 8-kyu/rental-car-cost.ts
@@ -0,0 +1,13 @@
+/*
+Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+*/
+
+export function rentalCarCost(d: number): number {
+  if (d >= 7) {
+    return d * 40 - 50;
+  } else if (d >= 3) {
+    return d * 40 - 20;
+  } else {
+    return d * 40;
+  }
+}
